7cd220d5ba51ebe89371a1d8b8f65d90d067df60
lalit adhikari <adhikarilalit15@gmail.com>
2024-12-02 22:11:01 +0530

    first commit

---
+++ src/Components/Offer.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Offer = () => {
+  return (
+    <div id="presentaion">
+      <div className="pr-heading">
+        <h1>
+          A BIG <span>OFFER</span> FOR THIS WINTER
+        </h1>
+        <p className="details">
+          Lorem ipsum dolor, sit amet consectetur adipisicing.
+        </p>
+        <div className="pr-btns">
+          <a href="#" className="pr-btn">
+            JOIN NOW
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Offer;
